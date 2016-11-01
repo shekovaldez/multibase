c29ac825d84bae20ce6b1b83773dad81ef2b8d2c
sheko <sheko.v.r@gmail.com>
2016-11-01 15:47:48 -0600

    Repositorio inicial

---
+++ Inicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio de Sesion</title>
+</head> 
+<body>
+<form action="Ingresar.php" method="POST" > <!-- hace referencia a ingresar.php por emetodo post-->
+<label>Email:</label>
+<input type="text" name="Email"><br><br>
+<label>Password:</label>
+<input type="password" name="Password"><br><br>
+<input type="submit" name="btIngresar" value="Ingresar">
+</form>
+
+</body>
+</html>
